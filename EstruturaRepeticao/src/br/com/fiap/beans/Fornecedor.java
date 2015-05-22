02c13449c73ee9831baec398c419c7511e3ebf7b
cesdouglas <cesdouglas@hotmail.com>
2015-05-22 11:46:57 -0300

    teste

---
+++ EstruturaRepeticao/src/br/com/fiap/beans/Fornecedor.java
@@ -0,0 +1,28 @@
+package br.com.fiap.beans;
+
+public class Fornecedor {
+	private String razaoSocial;
+	private String cnpj;
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public Fornecedor(String razaoSocial, String cnpj) {
+		super();
+		this.razaoSocial = razaoSocial;
+		this.cnpj = cnpj;
+	}
+	public Fornecedor() {
+		super();
+	}
+	
+	
+}
