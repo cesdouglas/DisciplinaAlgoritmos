2dd1348d7bfef1c89dcbb774dc4b79b7f9ad380e
cesdouglas <cesdouglas@hotmail.com>
2015-05-22 10:50:24 -0300

    teste

---
+++ EstruturaRepeticao/src/br/com/fiap/beans/Livro.java
@@ -0,0 +1,33 @@
+package br.com.fiap.beans;
+
+public class Livro extends Produto{
+	private String autor;
+	private String editora;
+	
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	
+	public Livro(String descricao, int codigo, double valor, String autor,
+			String editora) {
+		super(descricao, codigo, valor);
+		this.autor = autor;
+		this.editora = editora;
+	}
+	
+	public Livro() {
+		super();		
+	}
+	
+	
+	
+}
