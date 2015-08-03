c8eb3cd20e4ebad001e35fbec6603fc81cb935c3
cesdouglas <cesdouglas@hotmail.com>
2015-08-03 20:45:09 -0300

    submit

---
+++ EstruturaRepeticao/src/semestre2/alg/Exemplo_vetor.java
@@ -0,0 +1,22 @@
+package semestre2.alg;
+
+public class Exemplo_vetor {
+	public static void main(String[] args) {
+
+		//Declaracao e Instanciacao
+		int x[] = new int[3];
+		
+		//Declaracao, Instanciacao e Atribuicao
+		int y[] = {134, 32323, 132};
+		
+		//Atribuicao de Valores no Vetor X
+		x[0] = 20;
+		x[1] = 30;
+		x[2] = 40;
+		
+		System.out.println("Valor da Posicao 0 de X: " + x[0]);
+		System.out.println("Valor da Posicao 1 de X: " + x[1]);
+		System.out.println("Valor da Posicao 2 de X: " + x[2]);
+		
+	}	
+}
