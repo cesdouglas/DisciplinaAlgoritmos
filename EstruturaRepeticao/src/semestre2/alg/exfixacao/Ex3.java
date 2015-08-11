b3df1ff7f01038d7981bf2d467d7aec25cdbe3d5
cesdouglas <cesdouglas@hotmail.com>
2015-08-10 21:04:40 -0300

    ajuste ex3

--- EstruturaRepeticao/src/semestre2/alg/exfixacao/Ex3.java
+++ EstruturaRepeticao/src/semestre2/alg/exfixacao/Ex3.java
@@ -16,7 +16,7 @@ public class Ex3 {
 		}
 		
 		for(int i = 0; i<20; i++){
-			System.out.print("["+vetorResultante[i]+"], ");
+			System.out.print("["+vetorResultante[i]+ (i+1 == vetorResultante.length ? "]" : "], ") );
 		}
 
 	}
